8d397b20b1813b895f07a057212f43dcbf911347
KirillBorodinskiy <kirill.borodinskiy@gmail.com>
2024-12-01 17:39:40 +0100

    The most basic container for a calendar

---
+++ src/test/java/io/datajek/spring/basics/teamjob/TeamJobApplicationTests.java
@@ -0,0 +1,13 @@
+package io.datajek.spring.basics.teamjob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamJobApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
