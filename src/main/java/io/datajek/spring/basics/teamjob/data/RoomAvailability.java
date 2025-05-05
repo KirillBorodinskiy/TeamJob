47e9ffca7b51cc7fdb7b0b196cf3c3d60d4b026c
Kirill Borodinskiy <Kirill.borodinskiy@gmail.com>
2025-05-05 11:28:17 +0200

    Add models for room, user, and event availability with unoccupied time slots

---
+++ src/main/java/io/datajek/spring/basics/teamjob/data/RoomAvailability.java
@@ -0,0 +1,16 @@
+package io.datajek.spring.basics.teamjob.data;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+
+/**
+ * Represents the availability of a room with its unoccupied time slots.
+ */
+@Data
+@AllArgsConstructor
+public class RoomAvailability {
+    private Room room;
+    private ArrayList<TimeFromTo> unoccupiedTimesFromTo;
+}
