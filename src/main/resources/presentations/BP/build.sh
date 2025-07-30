c70c8e7cdf7b2cd1233ddd0a28cb2bd9348491d4
Kirill Borodinskiy <Kirill.borodinskiy@gmail.com>
2025-07-30 21:17:40 +0500

    Working state + install tutorial

--- src/main/resources/presentations/BP/build.sh
+++ src/main/resources/presentations/BP/build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+# 1. Delete auxiliary and temporary files
+rm -f *.aux *.bbl *.bcf *.blg *.lof *.log *.lot *.out *.toc
+
+# 2. Compile the document
+lualatex -shell-escape BP.tex
+biber BP
+lualatex -shell-escape BP.tex
 lualatex -shell-escape BP.tex
 
 
